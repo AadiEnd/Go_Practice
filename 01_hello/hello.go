3f5111645b9a14d57b792ad60d41458d88f9a303
Aadi Jain <aadijain024ap@gmail.com>
2026-08-27 22:39:51 +0530

    studying go

---
+++ 01_hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+var c, fun, car bool
+
+func main() {
+	i := 47
+	fmt.Println(i)
+}
---
+++ 02_sandbox/sandbox.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	fmt.Println("WELCOME TO THE PLAYGROUND")
+	fmt.Println("THE TIME IS : ", time.Now())
+}
