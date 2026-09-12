09916be0d29607d8d05e0d2979589af5337e6e3b
Aadi Jain <aadijain024ap@gmail.com>
2026-09-12 21:35:57 +0530

    function file

--- 24_func/myfunc.go
+++ 24_func/myfunc.go
@@ -1,6 +1,18 @@
 package main
-
 import "fmt"
+
+func switching(x , y int) (int, int){
+	
+	temp := x
+	x = y
+	y = temp
+	return x, y
+
+}
 func main(){
-	fmt.Println("don't wanna study today tomorrow for sure")
+	var1 := 10
+	var2 := 20
+	fmt.Printf("before swap: %v, %v ", var1, var2)
+	switching(var1, var2)
+	fmt.Printf("after swap: %v, %v \n ", var1, var2)
 }
